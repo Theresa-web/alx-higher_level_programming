02d2356b634c77e41a1f7387a127ba3f74b32c71
Theresa-web <73585864+Theresa-web@users.noreply.github.com>
2023-04-07 11:40:18 +0200

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,58 +1,33 @@
 #!/usr/bin/python3
-
+"""Unittest for max_integer([..])
+"""
 import unittest
-from 6-max_integer import max_integer
+max_integer = __import__('6-max_integer').max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
-    """
-    Unit tests for max_integer function.
-    """
-
     def test_empty_list(self):
         """
-        Test that an empty list returns None.
+        Test the max_integer function with an empty list.
         """
         self.assertIsNone(max_integer([]))
 
-    def test_single_item_list(self):
+    def test_one_item_list(self):
         """
-        Test that a list with one item returns that item.
+        Test the max_integer function with a list containing one item.
         """
-        self.assertEqual(max_integer([5]), 5)
+        self.assertEqual(max_integer([1]), 1)
 
-    def test_multiple_items_list(self):
+    def test_three_items_list(self):
         """
-        Test that a list with multiple items returns the maximum value.
+        Test the max_integer function with a list containing three items.
         """
         self.assertEqual(max_integer([1, 2, 3]), 3)
+        self.assertEqual(max_integer([2, 3, 1]), 3)
+        self.assertEqual(max_integer([3, 1, 2]), 3)
         self.assertEqual(max_integer([3, 2, 1]), 3)
-        self.assertEqual(max_integer([-1, -2, -3]), -1)
-        self.assertEqual(max_integer([-3, -2, -1]), -1)
-        self.assertEqual(max_integer([1, 1, 1]), 1)
 
     def test_floats(self):
-        """
-        Test that a list of floats returns the maximum value.
-        """
-        self.assertEqual(max_integer([1.53, 6.33, -9.123, 15.2, 6.0]), 15.2)
-
-    def test_strings(self):
-        """
-        Test that a list of strings raises TypeError.
-        """
-        with self.assertRaises(TypeError):
-            max_integer(["abc", "def", "ghi"])
-
-    def test_mixed_types(self):
-        """
-        Test that a list with mixed types raises TypeError.
-        """
-        with self.assertRaises(TypeError):
-            max_integer([1, 2, "abc", 3.14, -5])
-
-    def test_no_args(self):
-        """
-        Test that max_integer raises TypeError when called without arguments.
-        """
-        with self.assertRaises(TypeError):
-            max_integer()
+        """Test a list of floats."""
+        floats = [1.53, 6.33, -9.123, 15.2, 6.0]
+        self.assertEqual(max_integer(floats), 15.2)
