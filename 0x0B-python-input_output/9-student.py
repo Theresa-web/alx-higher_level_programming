5b5b9e37d8e5b3e608d67784bcef595dd2b4b37c
Theresa-web <73585864+Theresa-web@users.noreply.github.com>
2023-04-13 16:33:02 +0200

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+"""Defines a class Student."""
+
+
 class Student:
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        # Create a dictionary to store the serialized data
-        data = {}
-
-        # Serialize the instance attributes
-        data['first_name'] = self.first_name
-        data['last_name'] = self.last_name
-        data['age'] = self.age
-
-        return data
+        '''Get a dictionary representation of the Student.'''
+        return self.__dict__
