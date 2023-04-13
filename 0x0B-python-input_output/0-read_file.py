06b93fdf7e64084b9dd45b361f706fc217f7b068
Theresa-web <73585864+Theresa-web@users.noreply.github.com>
2023-04-13 16:09:04 +0200

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""text file-reading function."""
+
+
 def read_file(filename=""):
-    try:
-        with open(filename, mode="r", encoding="utf8") as f:
-            print(f.read())
-    except:
-        pass
+    """output the contents of a UTF8 text file to stout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
