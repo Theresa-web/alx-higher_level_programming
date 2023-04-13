5e5421e2a8dcaf1ef409b5654b1df3d0bc5bd7d7
Theresa-web <73585864+Theresa-web@users.noreply.github.com>
2023-04-13 16:27:54 +0200

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""JSON-to-object function."""
 import json
 
 def from_json_string(my_str):
+    '''Return object representation of a JSON string.'''
     return json.loads(my_str)
