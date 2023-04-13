f116c0ef04a67679d9228d241837e29a30c160f4
Theresa-web <73585864+Theresa-web@users.noreply.github.com>
2023-04-13 16:32:23 +0200

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
+"""Defines a Python class-to-JSON function."""
+
+
 def class_to_json(obj):
-    # Get all attributes of the object
-    attrs = vars(obj)
-
-    # Create a dictionary to store the serialized data
-    data = {}
-
-    # Iterate over the attributes and serialize them
-    for attr, value in attrs.items():
-        # Check if the value is serializable
-        if isinstance(value, (list, dict, str, int, bool)):
-            data[attr] = value
-
-    return data
+    '''Return the dictionary representation of a simple data structure.'''
+    return obj.__dict__
