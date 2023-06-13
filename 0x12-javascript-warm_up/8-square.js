dd730987a11c6b99d83588825f81da5fc27e3b9c
Theresa-web <73585864+Theresa-web@users.noreply.github.com>
2023-06-13 13:37:53 +0200

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
+let myStr = '';
 
 if (isNaN(myArgs[0])) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < parseInt(myArgs[0]); i++) {
-    console.log('C is fun');
+    myStr += 'X';
+  }
+  for (let j = 0; j < parseInt(myArgs[0]); j++) {
+    console.log(myStr);
   }
 }
